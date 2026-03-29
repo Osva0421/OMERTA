86ef85ed8fec321c162532a9a973e5cdfc03cbe6
Tu Nombre <osvaedu999@gmail.com>
2026-03-28 22:43:01 -0600

    Correcion

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -546,8 +546,8 @@ export default function InicioOMERTA() {
               <WomanCategoryRect title="Playera Oversize" subtitle="Comodidad sin esfuerzo" imgUrl="/oversize-woman.png" link={`/categoria/${crearSlug('woman-playeras-oversize')}`} />
               <WomanCategoryRect title="Hoodies" subtitle="Esenciales urbanos" imgUrl="/hoodie-woman.png" link={`/categoria/${crearSlug('woman-hoodies')}`} />
               <WomanCategoryRect title="Baby Tees" subtitle="Ajuste perfecto" imgUrl="/baby-tees-woman.png" link={`/categoria/${crearSlug('woman-baby-tees')}`} />
-              <WomanCategoryRect title="Accesorios" subtitle="Detalles que marcan la diferencia" imgUrl="/accesorios.png" link={`/categoria/${crearSlug('woman-accesorios')}`} />
-              <WomanCategoryRect title="Ver Todo" subtitle="Explora el archivo completo" imgUrl="/woman-all.jpg" link="/closet" />
+              <WomanCategoryRect title="Tiranres" subtitle="Detalles que marcan la diferencia" imgUrl="/tirantes-woman.png" link={`/categoria/${crearSlug('woman-tirantes')}`} />
+              <WomanCategoryRect title="Accesorios" subtitle="Explora el archivo completo" imgUrl="/accesorios.png" link={`/categoria/${crearSlug('accesorios')}`} />
             </div>
           </section>
         </>
